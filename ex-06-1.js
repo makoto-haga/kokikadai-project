b45e210727b5d161f3b903f6beefac0c1682cdbd
makoto-haga <ZM18023@josai.ac.jp>
2021-02-14 07:21:40 +0000

    0214

--- ex-06-1.js
+++ ex-06-1.js
@@ -1,1 +1,9 @@
-function imgchange1(){document.getElementById('logo').src='./image/tunku.jpg'}
+function imgchange1(){
+    document.getElementById('logo').src='./image/akimoto.jpg'
+}
+
+function show_innerHTML(){
+    var result= document.getElementById('logo').innerHTML;
+    alert(result);
+}
+
